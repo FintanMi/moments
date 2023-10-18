2aa6b2d8b70a6f57850139dd3191bf1a10755f55
FintanMi <>
2023-10-18 19:27:28 +0000

    added handler to close hamburger icon & refactored code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,13 @@ import {NavLink} from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
 import Avatar from './Avatar';
 import axios from 'axios';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+
+    const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
     const handleSignOut = async () => {
         try {
@@ -48,7 +51,7 @@ const NavBar = () => {
         </NavLink>
     </>
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to='/'>
         <Navbar.Brand>
@@ -56,7 +59,10 @@ const NavBar = () => {
         </Navbar.Brand>
         </ NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+            ref={ref}
+            onClick={() => setExpanded(!expanded)}
+            aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to='/'>
